7c72bd6027bc0f07955312d11bc66820691504d3
TienPoly <tnguyenduc86@gmail.com>
2018-11-19 09:59:16 -0500

    Update script_to_run.m

--- src/bag2txt_matlab/script_to_run.m
+++ src/bag2txt_matlab/script_to_run.m
@@ -1,4 +1,5 @@
 clc;close all; clear all
+file_name = {'razor2_syn'}
 % [names, types] = bagInfo('razor2_syn.bag')
 
 for i = 1:length(file_name)
@@ -8,8 +9,15 @@ x = vicon_pose.vicon_position_W_x;
 y = vicon_pose.vicon_position_W_y;
 psi = vicon_pose.vicon_euler_angles_z;
 
+figure()
+plot(x,y)
+
+%%
+A = [x y psi]
 fileID = fopen('vicon.txt','a');
 fprintf(fileID,'%10s %10s %10s\n','x','y','psi');
-fprintf(fileID,'%10.4f %10.4f %10.4f\n',x,y,psi);
+for i=1:length(A(:,1))
+    fprintf(fileID,'%10.4f %10.4f %10.4f \r\n',A(i,1),A(i,2),A(i,3));
+end
 fclose(fileID);
-% type vicon.txt
+% type result.txt
